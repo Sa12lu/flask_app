c1e49000ff11ec7a4b834199431245d20fe4b1f9
Nur Fatihah Ismail <saluhahsofia@gmail.com>
2025-06-18 21:47:49 +0800

    Switched to db4free.net MySQL

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
 # MySQL Configuration
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:@localhost/flask_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://psmflask:Sa2223lu@db4free.net/psmflask'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
